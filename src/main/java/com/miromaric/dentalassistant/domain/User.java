82d13e978f00e85aca0a78a62987d54b44daddbb
MiroMaric <miromaric1996@gmail.com>
2019-05-02 20:58:39 +0200

    Kreirana domenska klasa User

---
+++ src/main/java/com/miromaric/dentalassistant/domain/User.java
@@ -0,0 +1,86 @@
+package com.miromaric.dentalassistant.domain;
+
+/**
+ *
+ * @author MikoPC
+ */
+public class User {
+
+    private String username;
+    private String password;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String address;
+    private String phone;
+
+    public User() {
+    }
+
+    public User(String username, String password, String firstname, String lastname, String email, String address, String phone) {
+        this.username = username;
+        this.password = password;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+}
