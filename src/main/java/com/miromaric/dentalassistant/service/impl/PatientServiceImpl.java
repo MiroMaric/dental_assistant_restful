379296754ec155fb8e7ecf71768d159259c2bc51
MiroMaric <miromaric1996@gmail.com>
2019-05-06 19:46:47 +0200

    Kreirani PatientService interfejs i PatientServiceImpl klasa

---
+++ src/main/java/com/miromaric/dentalassistant/service/impl/PatientServiceImpl.java
@@ -0,0 +1,52 @@
+package com.miromaric.dentalassistant.service.impl;
+
+import com.miromaric.dentalassistant.dao.PatientDao;
+import com.miromaric.dentalassistant.dao.impl.PatientDaoImpl;
+import com.miromaric.dentalassistant.exception.ResourceNotFoundException;
+import com.miromaric.dentalassistant.model.Patient;
+import com.miromaric.dentalassistant.service.PatientService;
+import java.util.List;
+
+/**
+ *
+ * @author MikoPC
+ */
+public class PatientServiceImpl implements PatientService{
+
+    PatientDao dao = new PatientDaoImpl();
+    
+    @Override
+    public void save(Patient patient) {
+        dao.save(patient);
+    }
+
+    @Override
+    public List<Patient> getAll() {
+        return dao.getAll();
+    }
+
+    @Override
+    public Patient getOne(int id) {
+        Patient patient = dao.getOne(id);
+        if(patient!=null)
+            return patient;
+        throw new ResourceNotFoundException("Resource not found");
+    }
+
+    @Override
+    public Patient update(int id, Patient patient) {
+        Patient dbPatient = dao.update(id, patient);
+        if(dbPatient!=null)
+            return dbPatient;
+        throw new ResourceNotFoundException("Resource not found");
+    }
+
+    @Override
+    public Patient remove(int id) {
+        Patient patient = dao.remove(id);
+        if(patient!=null)
+            return patient;
+        throw new ResourceNotFoundException("Resource not found");
+    }
+    
+}
