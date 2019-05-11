68493cb0a253ac9e024dcf5f39f7b462c42a4d4e
MiroMaric <miromaric1996@gmail.com>
2019-05-12 00:18:08 +0200

    Zavrsena AppointmentResource klasa

--- src/main/java/com/miromaric/dentalassistant/AppointmentResource.java
+++ src/main/java/com/miromaric/dentalassistant/AppointmentResource.java
@@ -2,13 +2,19 @@ package com.miromaric.dentalassistant;
 
 import com.miromaric.dentalassistant.model.Appointment;
 import com.miromaric.dentalassistant.model.Patient;
+import com.miromaric.dentalassistant.model.dto.AppointmentReq;
 import com.miromaric.dentalassistant.myresponse.MyResponse;
 import com.miromaric.dentalassistant.myresponse.Status;
 import com.miromaric.dentalassistant.service.AppointmentService;
 import com.miromaric.dentalassistant.service.impl.AppointmentServiceImpl;
 import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -19,7 +25,7 @@ import javax.ws.rs.core.MediaType;
 @Path("appointments")
 public class AppointmentResource {
 
-    private AppointmentService service = new AppointmentServiceImpl();
+    private final AppointmentService service = new AppointmentServiceImpl();
     
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -27,5 +33,39 @@ public class AppointmentResource {
         List<Appointment> appointments =  service.getAll();
         return new MyResponse(Status.SUCCESS, appointments, null);
     }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{id}")
+    public MyResponse getOne(@PathParam("id")int id){
+        Appointment appointment = service.getOne(id);
+        return new MyResponse(Status.SUCCESS, appointment, null);
+    }
+    
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public MyResponse save(AppointmentReq appointmentReq){
+        Appointment appointment = appointmentReq.getModel();
+        service.save(appointment);
+        return new MyResponse(Status.SUCCESS, appointment, null);
+    }
+    
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{id}")
+    public MyResponse update(@PathParam("id")int id,AppointmentReq appointmentReq){
+        Appointment appointment = service.update(id, appointmentReq.getModel());
+        return new MyResponse(Status.SUCCESS, appointment, null);
+    }
+    
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{id}")
+    public MyResponse remove(@PathParam("id")int id){
+        Appointment appointment = service.remove(id);
+        return new MyResponse(Status.SUCCESS, appointment, null);
+    }
+    
 
 }
