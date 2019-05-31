287b0d1e250a5f793bf3c27b3b2edade6ac73c27
MiroMaric <miromaric1996@gmail.com>
2019-05-31 04:55:47 +0200

    Kreiran resurs InterventionResource

--- src/main/java/com/miromaric/dentalassistant/InterventionResource.java
+++ src/main/java/com/miromaric/dentalassistant/InterventionResource.java
@@ -1,6 +1,24 @@
 package com.miromaric.dentalassistant;
 
+import com.miromaric.dentalassistant.model.Intervention;
+import com.miromaric.dentalassistant.model.dto.InterventionDto;
+import com.miromaric.dentalassistant.myresponse.MyResponse;
+import com.miromaric.dentalassistant.myresponse.Status;
+import com.miromaric.dentalassistant.service.InterventionService;
+import com.miromaric.dentalassistant.service.impl.InterventionServiceImpl;
+import com.miromaric.dentalassistant.service.mapper.impl.InterventionMapperImpl;
+import java.util.List;
+import javax.validation.Valid;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -8,5 +26,48 @@ import javax.ws.rs.Path;
  */
 @Path("interventions")
 public class InterventionResource {
+    
+    private final InterventionService service = new InterventionServiceImpl();
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public MyResponse getAll(){
+        List<Intervention> interventions =  service.getAll();
+        return new MyResponse(Status.SUCCESS, interventions, null);
+    }
+    
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public MyResponse getOne(@PathParam("id")Long id){
+        Intervention intervention =  service.getOne(id);
+        return new MyResponse(Status.SUCCESS, intervention, null);
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response save(@Valid InterventionDto interventionDto){
+        service.save(new InterventionMapperImpl().toDataModel(interventionDto));
+        return Response.status(Response.Status.CREATED).entity(new MyResponse(Status.SUCCESS, interventionDto, null)).build();
+    }
+    
+    @DELETE
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public MyResponse remove(@PathParam("id")Long id){
+        Intervention intervention = service.remove(id);
+        return new MyResponse(Status.SUCCESS, intervention, null);
+    }
+    
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{id}")
+    public MyResponse update(@PathParam("id")Long id,@Valid InterventionDto interventionDto){
+        Intervention intervention = service.update(id,new InterventionMapperImpl().toDataModel(interventionDto));
+        return new MyResponse(Status.SUCCESS, intervention, null);
+    }
+    
     
 }
