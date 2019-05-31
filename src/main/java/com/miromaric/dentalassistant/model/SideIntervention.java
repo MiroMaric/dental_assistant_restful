53a39b02b8715d90729372c26ea21ac32ce9ed99
MiroMaric <miromaric1996@gmail.com>
2019-05-31 03:14:10 +0200

    Kreirana klasa SideIntervention koja nasledjuje InterventionItem klasu

---
+++ src/main/java/com/miromaric/dentalassistant/model/SideIntervention.java
@@ -0,0 +1,14 @@
+package com.miromaric.dentalassistant.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author MikoPC
+ */
+@Entity
+@Table(name="side_intervention")
+public class SideIntervention extends InterventionItem{
+    
+}
